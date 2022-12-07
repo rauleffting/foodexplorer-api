38eeaac68cdb068399f66452863438c8054514cf
Raul Francisco Effting <rauleffting94@gmail.com>
2022-12-07 15:18:35 -0300

    added some features

---
+++ src/controllers/IngredientsController.js
@@ -0,0 +1,12 @@
+const knex = require("../database/knex")
+
+class IngredientsController {
+  async index(request, response) {
+    const ingredients = await knex("ingredients")
+    .groupBy("name")
+
+    return response.json(ingredients);
+  }
+}
+
+module.exports = IngredientsController;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ const { Router } = require("express");
 
 const usersRoutes = require("./users.routes");
 const foodsRoutes = require("./foods.routes");
+const ingredientsRoutes = require("./ingredients.routes");
 const sessionsRoutes = require("./sessions.routes");
 
 const routes = Router();
 routes.use("/users", usersRoutes);
 routes.use("/foods", foodsRoutes);
+routes.use("/ingredients", ingredientsRoutes);
 routes.use("/sessions", sessionsRoutes);
 
 module.exports = routes;
